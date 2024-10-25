483076ce206ef8aa77574d38cc8fd1fc113119f4
Eduardolans <141261251+Eduardolans@users.noreply.github.com>
2024-10-25 20:34:16 +0200

    remove taskId from prisma schema; import Coremodule; migrate changes sql

--- back-end/src/opportunity/opportunity.module.ts
+++ back-end/src/opportunity/opportunity.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { OpportunityService } from './opportunity.service';
 import { OpportunityController } from './opportunity.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { CoreModule } from 'src/core/core.module';
 
 @Module({
+  imports: [CoreModule],
   controllers: [OpportunityController],
   providers: [OpportunityService, PrismaService],
 })
