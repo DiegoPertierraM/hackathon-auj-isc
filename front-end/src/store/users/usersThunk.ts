bd39bf3da8f30c43b2cef2ba989c2ad30287f6d6
Joselu <joseluissgz97@gmail.com>
2024-10-29 22:51:11 +0100

    feat: add thunk for adding task to user

--- front-end/src/store/users/usersThunk.ts
+++ front-end/src/store/users/usersThunk.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { AddUserToTask } from '../../components/specific-modals/addUserToTask/AddUserToTask';
 
 const baseUrl = import.meta.env.VITE_API_URL;
 
@@ -29,5 +30,34 @@ export const fetchUsers = createAsyncThunk('user/fetchUsers', async () => {
     return data;
   } catch (error) {
     return Promise.reject(error);
+  }
+});
+
+interface AddUserToTask {
+  taskId: number;
+  userId: number;
+}
+
+export const addTaskToUser = createAsyncThunk('user/post', async ({ taskId, userId }: AddUserToTask, thunkAPI) => {
+  try {
+    const token = localStorage.getItem('token')?.replace(/['"]/g, '');
+
+    const response = await fetch(`${baseUrl}user/${userId}/task/${taskId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if (!response.ok) {
+      return thunkAPI.rejectWithValue('Error en la petición');
+    }
+
+    const data = await response.json();
+    console.log({ data });
+    return data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue('Error de red o del servidor');
   }
 });
