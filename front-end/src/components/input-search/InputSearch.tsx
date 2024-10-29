c0bc3f06821dcc9f1602d179438060c53eff20d2
Joselu <joseluissgz97@gmail.com>
2024-10-29 18:32:06 +0100

    add: place holder to InputSearch

--- front-end/src/components/input-search/InputSearch.tsx
+++ front-end/src/components/input-search/InputSearch.tsx
@@ -2,12 +2,13 @@ import { IoSearchOutline } from 'react-icons/io5';
 import './inputSearch.scss';
 type Pops = {
   onSearch?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeHolder?: string;
 };
 
-export const InputSearch = ({ onSearch }: Pops) => {
+export const InputSearch = ({ placeHolder, onSearch }: Pops) => {
   return (
     <label className="search">
-      <input className="search__input" type="search" onChange={onSearch} />
+      <input className="search__input" type="search" onChange={onSearch} placeholder={placeHolder} />
       <IoSearchOutline className="search__icon" />
     </label>
   );
