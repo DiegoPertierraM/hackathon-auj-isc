8eba79602a70bfc4449b9ab0dbdbe82b2839c42c
Joselu <joseluissgz97@gmail.com>
2024-10-29 11:55:13 +0100

    add: Opportunities CRUD

--- front-end/src/store/opportunities/opportunitiesSlice.ts
+++ front-end/src/store/opportunities/opportunitiesSlice.ts
@@ -1,22 +1,83 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { opportunities } from '../../data/opportunities';
 import { Opportunity } from '../../interfaces/Opportunity.interface';
 import { RootState } from '../store';
+import { createOpportunity, deleteOportunity, getAllOpportunities, updateOportinity } from './opportunitiesThunk';
 
 interface OpportunityState {
   opportunities: Opportunity[];
   opportunity: Opportunity | null;
+  loading: 'idle' | 'loading' | 'succeeded' | 'failed';
+  error: string | null;
 }
 
 const initialState: OpportunityState = {
-  opportunities: opportunities,
-  opportunity: null
+  opportunities: [],
+  opportunity: null,
+  loading: 'idle',
+  error: null
 };
 
 export const opportunitiesSlice = createSlice({
   name: 'opportunities',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(getAllOpportunities.pending, state => {
+      state.loading = 'loading';
+      state.error = null;
+    });
+
+    builder.addCase(getAllOpportunities.fulfilled, (state, action) => {
+      state.loading = 'succeeded';
+      state.opportunities = action.payload;
+    });
+    builder.addCase(getAllOpportunities.rejected, (state, action) => {
+      state.loading = 'failed';
+      state.error = action.payload || 'Error desconocido';
+    });
+
+    builder.addCase(createOpportunity.pending, state => {
+      state.loading = 'loading';
+      state.error = null;
+    });
+    builder.addCase(createOpportunity.fulfilled, (state, action) => {
+      state.loading = 'succeeded';
+      state.opportunities.push(action.payload);
+    });
+    builder.addCase(createOpportunity.rejected, (state, action) => {
+      state.loading = 'failed';
+      state.error = action.payload || 'Error desconocido';
+    });
+
+    builder.addCase(deleteOportunity.pending, state => {
+      state.loading = 'loading';
+      state.error = null;
+    });
+    builder.addCase(deleteOportunity.fulfilled, (state, action) => {
+      state.loading = 'succeeded';
+
+      state.opportunities = state.opportunities.filter(opportunity => opportunity.id !== action.payload.id);
+    });
+    builder.addCase(deleteOportunity.rejected, (state, action) => {
+      state.loading = 'failed';
+      state.error = action.payload || 'Error desconocido';
+    });
+
+    builder.addCase(updateOportinity.pending, state => {
+      state.loading = 'loading';
+      state.error = null;
+    });
+    builder.addCase(updateOportinity.fulfilled, (state, action) => {
+      state.loading = 'succeeded';
+      console.log('UPDATE', action.payload);
+
+      // state.opportunities = state.opportunities.filter(opportunity => opportunity.id !== action.payload.id);
+    });
+    builder.addCase(updateOportinity.rejected, (state, action) => {
+      state.loading = 'failed';
+      state.error = action.payload || 'Error desconocido';
+    });
+  }
 });
 
 export const getOpportunities = (state: RootState) => state.opportunities.opportunities;
