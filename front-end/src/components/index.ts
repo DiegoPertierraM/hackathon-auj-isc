3e01c46d970a3d66eb680e080b4883e5316705cb
Joselu <joseluissgz97@gmail.com>
2024-10-29 22:51:43 +0100

    add: export AddUserToTask

--- front-end/src/components/index.ts
+++ front-end/src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './form-input/FormInput';
 export * from './input-search/InputSearch';
+export * from './specific-modals/addUserToTask/AddUserToTask';
 export * from './ui/header/Header';
 export * from './ui/side-bar/SideBar';
 export * from './ui/skeleton/Skleton';
