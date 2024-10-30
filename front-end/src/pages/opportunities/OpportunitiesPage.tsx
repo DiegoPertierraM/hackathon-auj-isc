3dcf3a53620993230252d5201741a33b1d5fd0e1
Joselu <joseluissgz97@gmail.com>
2024-10-30 19:07:15 +0100

    update: change task filter placeHolder and add actions to opportunities

--- front-end/src/pages/opportunities/OpportunitiesPage.tsx
+++ front-end/src/pages/opportunities/OpportunitiesPage.tsx
@@ -80,6 +80,7 @@ export const OpportunitiesPage = () => {
                 <th>Titulo</th>
                 <th>Description</th>
                 <th>Estado</th>
+                <th>Acciones</th>
               </tr>
             </thead>
             <tbody>
